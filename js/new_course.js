c680298826874d5d7a4a1503f61e6f778ed70a2b
Mohammed Sherin-mv202212598 <mohammedsherinofficial@gmail.com>
2025-04-12 04:43:31 +0300

    fix: update course descriptions and add partially working POST function for new course

--- js/new_course.js
+++ js/new_course.js
@@ -5,13 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const addPrerequisiteBtn = document.querySelector("#addPrerequisite");
   const prerequisitesContainer = document.querySelector("#prerequisites");
   const cancelBtn = document.querySelector(".btn-cancel");
-  const instructorDD = document.querySelector("sections0.instructor");
-
-  instructorDD.addEventListener("click", async function () {
-    // const instructors = await fetch("http://localhost:3000/api/instructors");
-    // const instructorsData = await instructors.json();
-    console.log("bye bye");
-  });
 
   let sectionCounter = 1;
 
@@ -129,6 +122,13 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(courseData);
 
     alert("Course created successfully!\n");
+    fetch(`http://localhost:3000/api/courses`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(courseData),
+    });
     // location.reload();
   });
 
