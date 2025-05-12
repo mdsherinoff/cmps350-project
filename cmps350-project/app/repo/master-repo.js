9de36fcec210670be85843a71b36d8b01c4d18eb
Tytexx <195688302+mishal-ma2308960@users.noreply.github.com>
2025-05-12 18:38:53 +0300

    Login Fixed

--- cmps350-project/app/repo/master-repo.js
+++ cmps350-project/app/repo/master-repo.js
@@ -2,11 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 class MasterRepo {
-<<<<<<< Updated upstream
   async getAllUsers() {
-=======
-  async getUsers() {
->>>>>>> Stashed changes
     return await prisma.user.findMany({
       orderBy: { username: "asc" },
     });
@@ -19,7 +15,6 @@ class MasterRepo {
       },
     });
   }
-<<<<<<< Updated upstream
   async getAllCourses() {
     return await prisma.course.findMany({
       orderBy: { code: "asc" },
@@ -48,15 +43,6 @@ class MasterRepo {
     });
   }
 
-=======
-
-  async getUserbyId(id) {
-    return await prisma.user.findUnique({
-      where: { id },
-    });
-  }
-
->>>>>>> Stashed changes
   async findUserByUsername(username) {
     return await prisma.user.findUnique({
       where: { username },
@@ -537,16 +523,6 @@ class MasterRepo {
       topStudentsByGPA,
       studentsByYear,
     };
-  }
-
-  async getAllSections() {
-    return await prisma.section.findMany({
-      include: { course: true, instructor: true },
-    });
-  }
-
-  async createSection(data) {
-    return await prisma.section.create({ data });
   }
 }
 
