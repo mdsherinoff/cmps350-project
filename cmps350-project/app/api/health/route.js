3488983978c982aabc778ab65119bff558ebf8c7
Mohammed Sherin-mv202212598 <mohammedsherinofficial@gmail.com>
2026-06-21 22:55:27 +0300

    Setup: /api/health

---
+++ cmps350-project/app/api/health/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json(
+    { status: "ok" },
+    {
+      status: 200,
+      headers: { "Access-Control-Allow-Origin": "*" },
+    },
+  );
+}
